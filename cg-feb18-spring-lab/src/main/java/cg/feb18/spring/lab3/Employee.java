a478dc58db0c53328f2690326c1591ba8ddace4c
Srikanth Ghattamaneni <ghattamanenisrikanth8@gmail.com>
2022-03-23 18:16:33 +0530

    Spring

---
+++ cg-feb18-spring-lab/src/main/java/cg/feb18/spring/lab3/Employee.java
@@ -0,0 +1,11 @@
+package cg.feb18.spring.lab3;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	private double salary;
+	private int age;
+}
+	
